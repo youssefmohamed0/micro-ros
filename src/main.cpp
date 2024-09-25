45b6e7bac217666b2f0065ec5277907531162b6f
youssefmohamed0 <youssefyoussef.m4@gmail.com>
2024-09-25 23:51:09 +0300

    modify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ rcl_allocator_t allocator;
 rcl_node_t node;
 rcl_timer_t timer;
 
-// #define LED_PIN 5
+
 
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
@@ -54,19 +54,9 @@ void subscription_callback(const void * msgin)
     // right
 }
 
-// void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
-//   RCLC_UNUSED(last_call_time);
-//   if (timer != NULL) {
-//     pub_msg.data = 2;
-//     RCSOFTCHECK(rcl_publish(&publisher, &pub_msg, NULL));
-//   }
-// }
 
 
 void setup() {
-  //set wifi transport
-
-  // set_microros_transports();
 
   // Serial.begin(115200);
   // set_microros_serial_transports(Serial);
@@ -103,28 +93,12 @@ void setup() {
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),"key"));
 
-  // //create publisher
-  // RCCHECK(rclc_publisher_init_default(
-  //   &publisher,
-  //   &node,
-  //   ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-  //   "test_pub"));
-
-  // // create timer,
-  // const unsigned int timer_timeout = 1000;
-  // RCCHECK(rclc_timer_init_default(
-  //   &timer,
-  //   &support,
-  //   RCL_MS_TO_NS(timer_timeout),
-  //   timer_callback));
 
   //create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
   // RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-  // pub_msg.data = 0;
-  
 
 }
 
